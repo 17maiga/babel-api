01098cb18dee76a015bc10f2b35fa4ac21422f34
Henri Saudubray <henri.saudubray@gmail.com>
2023-03-13 15:35:47 +0100

    Update routes

--- index.js
+++ index.js
@@ -5,35 +5,29 @@ const library = require("./libraryofbabel");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/api/page/:address", (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({
-    "content": "Hello, world!"
-  }));
-});
-
-app.get("/read/:address", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
+  console.log("Received request for page: " + req.params["address"]);
   res.send(JSON.stringify({
     "page": library.getPage(req.params["address"])
   }));
 });
 
-app.get("/find/", (req, res) => {
+app.get("/api/find/", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({
     "address": library.search(req.body)
   }));
 });
 
-app.get("/find-title/", (req, res) => {
+app.get("/api/find-title/", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({
     "address": library.searchTitle(req.body)
   }));
 });
 
-app.get("/get-title/:address", (req, res) => {
+app.get("/api/get-title/:address", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({
     "title": library.getTitle(req.params["address"])
